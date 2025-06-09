146fca18050fb61f44a9b3aa1fbec592e51a1bcf
Danish <khandanish30599@gmail.com>
2025-06-09 15:03:20 +0530

    fix

--- src/app/RateCalculator/page.js
+++ src/app/RateCalculator/page.js
@@ -159,7 +159,7 @@ export default function Page() {
               <div>
                 <label className=" text-gray-700 font-medium mb-2 flex items-center justify-between">
                   <span>Package Type</span>
-                  <button
+                  {/* <button
                     type="button"
                     onClick={() => {
                       setShowAdvanced(true);
@@ -172,7 +172,7 @@ export default function Page() {
                     className="text-indigo-600 text-sm font-semibold underline hover:text-indigo-800 transition"
                   >
                     + Advanced Package
-                  </button>
+                  </button> */}
                 </label>
                 <select
                   className="w-full border border-indigo-300 rounded-xl px-5 py-3 text-gray-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 transition"
